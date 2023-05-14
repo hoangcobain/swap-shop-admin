f8ba0c85b49056bd73578c7f8c041cccb05494a1
hoangcobain <hoang21062001@gmail.com>
2023-05-14 22:39:35 +0700

    config deploy

--- src/libs/graphqlClient.ts
+++ src/libs/graphqlClient.ts
@@ -1,8 +1,11 @@
 import { GraphQLClient } from 'graphql-request';
 
-const API_URL = process.env.REACT_APP_GRAPHQL_URL as string;
+const API_URL =
+    process.env.NODE_ENV === 'production'
+        ? process.env.REACT_APP_GRAPHQL_URL
+        : ('http://localhost:4000/graphql' as string);
 
-const graphQLClient = new GraphQLClient(API_URL, {
+const graphQLClient = new GraphQLClient(API_URL as string, {
     // headers: {
     // Authorization: `Bearer ${process.env.API_KEY}`,
     // },
